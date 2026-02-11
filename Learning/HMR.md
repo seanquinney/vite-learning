4bb689e14eef5657a16ab5bffe14cfa0eae6edbc
Sean Quinney <sean97quinney@gmail.com>
2026-02-11 20:00:30 +0000

    Added images to the webpage
    Moved javascript icon into assets folders. Also learned about assets existing in the public folder vs src/assets.
    
    Assets get optimised in src/assets, whereas they don't in public.

--- Learning/HMR.md
+++ Learning/HMR.md
@@ -1,1 +1,2 @@
 Hot Module Replacement or HMR allows us to add more components to our webpage while it is live without restarting the app and losing the state.
+ES modules = standardised format for packing Javascript code for reuse.
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import javascriptLogo from './assets/javascript.svg' // exists in src folder, imported as a module
+import viteLogo from '/vite.svg' // exists in public folder
 
 export default function App() {
   return (
     <div>
       <Header />
       <h1>Hello React + Vite 🚀</h1>
+      <img src={javascriptLogo} className="logo" alt="JavaScript logo" />
+      <img src={viteLogo} className="logo" alt="Vite logo" />
       <Footer />
     </div>
   )
