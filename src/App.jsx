2d2103c12ea742fd89ba3176e2cafe062254f48d
Sean Quinney <sean97quinney@gmail.com>
2026-02-16 18:56:18 +0000

    Added environment variable inside .env and displayed it on the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import javascriptLogo from './assets/javascript.svg' // exists in src folder, im
 import viteLogo from '/vite.svg' // exists in public folder
 
 export default function App() {
+  const greeting = import.meta.env.VITE_GREETING
+  
   return (
     <div>
       <Header />
       <h1>Hello React + Vite 🚀</h1>
+      <h2>{greeting}</h2>
       <img src={javascriptLogo} className="logo" alt="JavaScript logo" />
       <img src={viteLogo} className="logo" alt="Vite logo" />
       <Footer />
